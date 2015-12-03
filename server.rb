d8467186526c36212715d603aff924e7b7a2a0a7
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-03 00:38:31 -0500

    layout, index, server paths, some styling

--- server.rb
+++ server.rb
@@ -1,9 +1,49 @@
 module App
   class Server < Sinatra::Base
   
+    set :method_override, true
+    enable :sessions
+  
     get "/" do
+      @topics = Topic.all
+      erb :index
+    end
+
+    get "/login" do
+      "Hello World"
+    end
+
+    post "/login" do
+      "Hello World"
+    end
+
+    get "/signup" do
+      "Hello World"
+    end
+
+    post "/signup" do
+      "Hello World"
+    end
+
+    get "/topic/:id" do
       "Hello World"
     end
     
+    get "/topic/new" do
+      "Hello World"
+    end
+
+    get "/topic/:id/comment/new" do
+      "Hello World"
+    end
+
+    post "/topic/:id/comment" do
+      "Hello World"
+    end
+
+    delete "/logout" do
+      "Hello World"
+    end
+
   end
 end

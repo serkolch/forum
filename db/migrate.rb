b534f038cbbf60a8c0701459bc11bc36298a1b33
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-07 09:28:20 -0500

    fix migration file

--- db/migrate.rb
+++ db/migrate.rb
@@ -1,8 +1,8 @@
-require "migrations/create_comments.rb"
-require "migrations/create_tags.rb"
-require "migrations/create_tags_topics.rb"
-require "migrations/create_topics.rb"
-require "migrations/create_users.rb"
+require_relative "migrations/create_comments.rb"
+require_relative "migrations/create_tags.rb"
+require_relative "migrations/create_tags_topics.rb"
+require_relative "migrations/create_topics.rb"
+require_relative "migrations/create_users.rb"
 
 CreateComments.migrate(ARGV[0])
 CreateTags.migrate(ARGV[0])

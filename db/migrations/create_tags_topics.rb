ddba4ec5a7562e13ef7d7c5c7f51b170a3092829
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-07 10:18:03 -0500

    topic view no-method error on .id

--- db/migrations/create_tags_topics.rb
+++ db/migrations/create_tags_topics.rb
@@ -2,7 +2,7 @@ require_relative "../config"
 
 class CreateTagsTopics < ActiveRecord::Migration
   def up
-    create_table :tags_topics do |t|
+    create_table :tags_topics, id: false do |t|
       t.integer(:tag_id)
       t.integer(:topic_id)
     end
